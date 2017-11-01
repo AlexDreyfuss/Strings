0c9a6d78a2fe6c4fef897d6a290a28a61b4ee9ef
Alexandre Dreyfuss <arele94@gmail.com>
2017-11-01 16:45:36 +0200

    Write my own String class

---
+++ Strings/MyStrings.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <iostream>
+using namespace std;
+ // this is a custom string class which has string features
+//1. GetLength() -get lenth of string
+//2. Assign() - set a new string 
+//3. CharAt() - get char at given index
+//4. Append() - append string to current 
+//5. Compare() between two strings
+//6. IsEmpty() - check if cuurent is empty
+//7. Clear(); - clears the current string
+class MyStrings 
+{
+	char * theString;
+	size_t mysize;
+public:
+	MyStrings();
+	MyStrings(char * str);
+	~MyStrings();
+	size_t GetLength();
+	char* Assign(MyStrings* otherString);
+	char CharAt(size_t index);
+	char* Append(MyStrings* otherString);
+	bool Compare(MyStrings* otherString);
+	bool IsEmpty();
+	void Clear();
+};
